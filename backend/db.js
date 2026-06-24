6d9b7a0c58e932df284111701f3ddf31e233d1cb
Jayanth <jayanthjai347@gmail.com>
2026-06-24 15:59:26 +0530

    Use Neon connection string

--- backend/db.js
+++ backend/db.js
@@ -3,18 +3,10 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-   ssl: {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
     rejectUnauthorized: false
   }
-});
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
 });
 
 module.exports = { pool };
