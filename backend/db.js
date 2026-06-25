0a4deb8686a2829a6a14fd01657ebc8e02baff99
Jayanth <jayanthjai347@gmail.com>
2026-06-25 10:40:16 +0530

    Check DATABASE_URL

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
+console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL);
 const { Pool } = require('pg');
 
 const pool = new Pool({
